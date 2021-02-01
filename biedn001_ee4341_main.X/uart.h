bc127878fd7bcd978c74d79ef4fb662f174d54fc
Alex Biedny <abiedny09@gmail.com>
2021-02-01 15:27:34 -0600

    Create uart.h

---
+++ biedn001_ee4341_main.X/uart.h
@@ -0,0 +1,15 @@
+#ifndef _UART_H    /* Guard against multiple inclusion */
+#define _UART_H
+
+#include <xc.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/attribs.h>
+
+void uart1_setup(void);
+
+#endif /* _UART_H */
+
+/* *****************************************************************************
+ End of File
+ */
